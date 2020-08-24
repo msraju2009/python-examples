0f02ec5a56ab63408fe7edd84a1e279ec533c717
vishnu-raju-96 <70016216+vishnu-raju-96@users.noreply.github.com>
2020-08-24 17:20:17 +0530

    Update drop_down_automation.py

--- drop_down_automation.py
+++ drop_down_automation.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-driver=webdriver.Chrome(r"path\chromedriver.exe")
+driver=webdriver.Chrome(r"add chrome path\chromedriver.exe")
 driver.get("url")
 element=driver.find_element_by_xpath("xpath")
 x=Select(element)
