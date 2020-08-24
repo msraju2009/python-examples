27893fa53bf1505e6932809dcf7333437fd82faf
vishnu-raju-96 <70016216+vishnu-raju-96@users.noreply.github.com>
2020-08-24 17:19:47 +0530

    added dropdown file

---
+++ drop_down_automation.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+driver=webdriver.Chrome(r"path\chromedriver.exe")
+driver.get("url")
+element=driver.find_element_by_xpath("xpath")
+x=Select(element)
+x.select_by_visible_text("text")
+x.select_by_index(index)
+x.select_by_value("value in html")
